6602f19dd8b55989e5164be0595e38d5b2262be5
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-03-04 02:49:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ dotenv.config({ path: "config.env" });
 if (process.env.NODE_ENV === "dev") {
   app.use(morgan("dev"));
 }
-app.post("/webhook",express.raw({ type: 'application/json' }),webhookCheckout)
 app.use(express.json());
+app.post("/webhook",express.raw({ type: 'application/json' }),webhookCheckout)
 app.use(express.static(path.join(__dirname,'uploads')))
 databaseConect();
 
