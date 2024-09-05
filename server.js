74852f545fec14f066ef5de7e755fcce4f18d7f5
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-09-05 09:47:25 +0300

    fix app problem

--- server.js
+++ server.js
@@ -16,6 +16,34 @@ const { webhookCreateOrder } = require("./controllers/orderController");
 
 const app = express();
 
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res, next) => {
+    const sig = req.headers["stripe-signature"];
+    let event;
+    try {
+      // Ensure req.body is used for signature verification
+      event = await Stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.stripe_webhook
+      );
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    if (event.type === "checkout.session.completed") {
+      webhookCreateOrder(event.data.object);
+    }
+
+    res.status(200).json({ received: true });
+  }
+);
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "uploads")));
+
 dotenv.config({ path: "config.env" });
 
 if (process.env.NODE_ENV === "dev") {
@@ -59,33 +87,6 @@ databaseConect();
 serverRoutes(app);
 
 // Middleware for parsing raw JSON requests for Stripe webhook
-app.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (req, res, next) => {
-    const sig = req.headers["stripe-signature"];
-    let event;
-    try {
-      // Ensure req.body is used for signature verification
-      event = await Stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        process.env.stripe_webhook
-      );
-    } catch (err) {
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-    if (event.type === "checkout.session.completed") {
-      webhookCreateOrder(event.data.object);
-    }
-
-    res.status(200).json({ received: true });
-  }
-);
-
-app.use(express.json({ limit: "500kb" }));
-
-app.use(express.static(path.join(__dirname, "uploads")));
 
 
 // Handle all other routes
